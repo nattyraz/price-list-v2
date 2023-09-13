b968d07893fda9718b0ce632d9e6bc279758529b
ROFOX <rgfogliami@gmail.com>
2023-09-13 11:55:08 +0200

    Delete pages/premium.py

--- pages/premium.py
+++
@@ -1,9 +0,0 @@
-
-import streamlit as st
-import pandas as pd
-
-def app():
-    st.title('Premium')
-    excel_data = pd.read_excel("path_to_excel_file.xlsx")
-    filtered_data = excel_data[excel_data['Condition'] == 'Premium']
-    st.write(filtered_data)
